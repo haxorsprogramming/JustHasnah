7ce60c8ac37075c2fa4727ba3148e16d177761e9
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-06-07 00:46:38 +0700

    enggak dong

--- index.php
+++ index.php
@@ -4,28 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello Brada ... :)</title>
+    <title>Hello bradaa... :-)</title>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=The+Girl+Next+Door&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Gaegu&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
     <script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
     <link href='css/style.css' rel='stylesheet'>
 </head>
 
 <body>
-    <div style='text-align:center;margin-top:20px;' class="container">
-        <img src='img/my.jpg' class="circle">
+    <div style='text-align:center;margin-top:20px;border-radius:2px #ecf0f1 solid;' class="container" id='divUtama'>
+        <img src='img/my.jpg' class="circle" style="border:7px solid #26a69a;">
         <h3 class="light" id='txtHeader'></h3>
         <p class="" id='txtDeks'></p>
-
     </div>
     <div> </div>
     <div class="container">
@@ -72,33 +69,87 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col s12 l4" style="padding-top:12px;">
-                
-            </div>
-            <div class="col s12 l4" style="padding-top:12px;">
-            <p>My Write</p>
+
+                <p>My Passion</p>
                 <div class="row">
-                        <div class="col s12 m12 l12">
-                            <div class="card" >
-                                <div class="card-image">
-                                    <img src="img/mm.jpg">
-                                    <span class="card-title indigo-text">Card Title</span>
-                                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i
-                                            class="material-icons">chrome_reader_mode</i></a>
-                                </div>
-                                <div class="card-content">
-                                    <p>I am a very simple card. I am good at containing small bits of information. I am
-                                        convenient because I require little markup to use effectively.</p>
-                                </div>
+                    <div class="col s6">Writing</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 70%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Bacot bacot</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 100%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Travelling</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 50%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Photography</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 80%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Mengkhayal</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 100%"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col s12 l4" style="padding-top:12px;" id='divWorks'>
+                <p>My Works & Portfolio</p>
+                <ul class="collapsible">
+                    <li v-for='ls in workList'>
+                        <div class="collapsible-header"><i class="material-icons">filter_vintage</i>{{ls.title}}</div>
+                        <div class="collapsible-body">
+                            <img src='img/works/nadha_laundry.jpg' style="width: 150px;"><br/>    
+                        <span>{{ls.teks}}</span>
+                        </div>
+                    </li>
+                </ul>
+                <p>
+                    See my work on my <a href='https://github.com/nadhaalditha' target="new">github</a>
+                </p>
+            </div>
+            <div class="col s12 l4" style="padding-top:12px;" id='divWrite'>
+                <p>My Write</p>
+                <div class="row" v-for='wl in workWrite'>
+                    <div class="col s12 m12 l12">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="img/pos/pos_1.jpg">
+                                <span class="card-title white-text" style='background-color: rgba(38, 166, 154, 0.7);'>{{wl.title}}</span>
+                                <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                        class="material-icons">chrome_reader_mode</i></a>
+                            </div>
+                            <div class="card-content">
+                                <p>{{wl.teks}}</p>
                             </div>
                         </div>
-                    
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-<script src='js/hasnah.js'></script>
+    <script src='js/hasnah.js'></script>
 </body>
 
 </html>
--- js/hasnah.js
+++ js/hasnah.js
@@ -1,5 +1,29 @@
+var divWorks = new Vue({
+    el : '#divWorks',
+    data :  {
+        workList : [
+            {title : 'Nadha Laundry', teks : 'Application for managing laundry businesses'},
+            {title : 'Kede Kita', teks : 'Application for managing small shop finances'},
+            {title : 'IRA - Marsjuanda Clinic', teks : 'Image processing application for disease identification needs'},
+            {title : 'Nadha Resto', teks : 'Application for managing restaurant businesses'},
+            {title : 'IRA - Trianda Hospital', teks : 'Image processing application for disease identification needs'},
+        ]
+    }
+});
+
+var divWrite = new Vue({
+    el : '#divWrite',
+    data : {
+        workWrite : [
+            {title : 'Lu bukan kiyubi bangsat...', teks : 'Eh goblok ... gak perlu marah marah ..'},
+            {title : 'Mouse komputer itu banyak halunya ...', teks : 'Eh goblok ... gak perlu marah marah ..'}
+        ]
+    }
+});
+
 $(document).ready(function(){
     console.log("All unit please response for ready..");
+    $('.collapsible').collapsible();
 });
 
 $('#divBody').hide();
@@ -12,12 +36,11 @@ new TypeIt("#txtHeader", {
     }
   }).go();
 
- 
 
   function teksKedua()
   {
     new TypeIt("#txtDeks", {
-        strings: " * Front End Dev | * Product Designer | * Opensource Addict | * Noob Writer",
+        strings: "  Front End Dev |  Product Designer |  Opensource Addict | Noob Writer",
         speed: 50,
         waitUntilVisible: true,
         afterComplete: async (step, instance) => {
