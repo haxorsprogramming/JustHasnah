16dacf10a388737b61bfd7bcbee0b43e27e4f10b
Haxorsprogramming <haxorsuinsu@gmail.com>
2020-06-06 13:33:22 +0700

    anashita

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Brada ... :)</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=The+Girl+Next+Door&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <link href='css/style.css' rel='stylesheet'>
+</head>
+
+<body>
+    <div style='text-align:center;margin-top:20px;border:2px #1abc9c solid; border-radius : 20px;' class="container">
+        <img src='img/my.jpg' class="circle">
+        <h3 class="light">Hi . . . Iam Hasnah</h3>
+        <p class="">
+            * Front End Dev | * Product Designer | * Opensource Addict | * Noob Writer
+        </p>
+
+    </div>
+    <div> </div>
+    <div class="container">
+        <div class="row center-align" style='margin-top:30px;border:2px #1abc9c solid; border-radius : 20px;'>
+            <div class="col s12 l4" style="padding-top:12px;">
+                <p>My Skills</p>
+                <div class="row">
+                    <div class="col s6">HTML - CSS</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 80%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Vue Js</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 75%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">React Js</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 40%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Figma</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 60%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s6">Photoshop</div>
+                    <div class="col s6">
+                        <div class="progress">
+                            <div class="determinate" style="width: 70%"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 l4" style="padding-top:12px;">
+                <p>My Porfolio</p>
+                <div class="row">
+                        <div class="col s12 m12 l12">
+                            <div class="card" >
+                                <div class="card-image">
+                                    <img src="img/mm.jpg">
+                                    <span class="card-title">Card Title</span>
+                                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                                            class="material-icons">chrome_reader_mode</i></a>
+                                </div>
+                                <div class="card-content">
+                                    <p>I am a very simple card. I am good at containing small bits of information. I am
+                                        convenient because I require little markup to use effectively.</p>
+                                </div>
+                            </div>
+                        </div>
+                    
+                </div>
+            </div>
+            <div class="col s12 l4" style="padding-top:12px;">
+                <p>My Write</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
